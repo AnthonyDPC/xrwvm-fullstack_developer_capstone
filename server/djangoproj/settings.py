b70f557215d46575b5876eb4605cff4093eb8fc4
Anthony de Paula <anthonydepaulacruz@gmail.com>
2025-03-29 22:13:24 -0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://anthonydepau-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
+
 CSRF_TRUSTED_ORIGINS = [
     'https://anthonydepau-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
